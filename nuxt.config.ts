2ff7fd537924c56b88e1eaddc3ea4ed1ae9da3ab
ivanZarza <ivanzarzaestevez@gmail.com>
2025-06-08 18:02:12 +0200

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+
+  app: {
+    baseURL: '/QuilinoReyes/'
+  },
 
   modules: [
     '@nuxt/fonts',
