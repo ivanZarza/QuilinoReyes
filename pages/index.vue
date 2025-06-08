8a0dea556f1a96f7addb28bba2567161815c48fd
ivanZarza <ivanzarzaestevez@gmail.com>
2025-06-08 17:39:02 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -77,7 +77,7 @@
   color: #fff;
   font-size: 1.25rem;
   margin-bottom: 2rem;
-  text-align: center;
+  text-align: center; 
   box-shadow: 0 2px 16px 0 rgba(0,0,0,0.2);
   border-bottom: 3px solid goldenrod;
   font-family: 'Montserrat', Arial, sans-serif;
