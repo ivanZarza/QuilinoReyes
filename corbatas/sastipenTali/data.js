41fdecb2793fdcfdaf5f28ad22530185df07c07a
ivanZarza <ivanZarza@users.noreply.github.com>
2025-06-15 14:55:27 +0000

    deploy: a96607f3fb7d4528dc40e6d6916f1d207118c4d7

--- corbatas/sastipenTali/data.js
+++ corbatas/sastipenTali/data.js
@@ -2,52 +2,52 @@ export const items2 = [
   {
     id: 13,
     nombre: "nombre1",
-    fotos: ["../sastipenTali/nombre1.jpeg"],
+    fotos: ["nombre1.jpeg"],
     precio: "25 Euros"
   },
   {
     id: 14,
     nombre: "nombre2",
-    fotos: ["../sastipenTali/nombre2.jpeg"],
+    fotos: ["nombre2.jpeg"],
     precio: "25 Euros"
   },
   {
     id: 15,
     nombre: "nombre4",
     fotos: [
-      "../sastipenTali/nombre4a.jpeg",
-      "../sastipenTali/nombre4b.jpeg"
+      "nombre4a.jpeg",
+      "nombre4b.jpeg"
     ],
     precio: "25 Euros"
   },
   {
     id: 16,
     nombre: "nombre5",
-    fotos: ["../sastipenTali/nombre5.jpeg"],
+    fotos: ["nombre5.jpeg"],
     precio: "25 Euros"
   },
   {
     id: 17,
     nombre: "nombre6",
-    fotos: ["../sastipenTali/nombre6.jpeg"],
+    fotos: ["nombre6.jpeg"],
     precio: "25 Euros"
   },
   {
     id: 18,
     nombre: "nombre7",
-    fotos: ["../sastipenTali/nombre7.jpeg"],
+    fotos: ["nombre7.jpeg"],
     precio: "25 Euros"
   },
   {
     id: 19,
     nombre: "nombre8",
-    fotos: ["../sastipenTali/nombre8.jpeg"],
+    fotos: ["nombre8.jpeg"],
     precio: "25 Euros"
   },
   {
     id: 20,
     nombre: "nombre9",
-    fotos: ["../sastipenTali/nombre9.jpeg"],
+    fotos: ["nombre9.jpeg"],
     precio: "25 Euros"
   }
 ];
