8f06039ebe2f7b4ed5d650a0494a0deb97e21041
ivanZarza <ivanZarza@users.noreply.github.com>
2025-06-15 08:30:05 +0000

    deploy: aa864a046d19cecd146d3488ff8c6e1987cb6cb3

---
+++ moda hombre/data.js
@@ -0,0 +1,11 @@
+export const items4 = [
+  {
+    id: 27,
+    nombre: "nombre1",
+    descripcion: "",
+    fotos: [
+      "../modaHombre/nombre1a.jpeg",
+      "../modaHombre/nombre1b.jpeg"
+    ]
+  }
+]
