d17200bfd44ad42d151afbe2c722abbe0c6ecfe7
ivanZarza <ivanZarza@users.noreply.github.com>
2025-06-16 13:15:18 +0000

    deploy: a2de297b9fcfcbc04bd42a3acdbd8818d376ec82

--- _nuxt/CaNKaz2b.js
+++
@@ -1,1 +0,0 @@
-import{v as o,c as r,a as e,t as s,j as n,o as p}from"./9Z0ZTIan.js";const d={__name:"[temporada]",setup(l){const t=o().params.temporada;return(m,a)=>(p(),r("div",null,[e("h2",null,"Temporada: "+s(n(t)),1),a[0]||(a[0]=e("p",null,"Aquí puedes mostrar las corbatas de la temporada seleccionada.",-1))]))}};export{d as default};
