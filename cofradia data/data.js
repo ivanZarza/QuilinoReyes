78f1f7e0c84b9985a493828af6f503fa67bf1945
ivanZarza <ivanZarza@users.noreply.github.com>
2025-06-17 14:46:58 +0000

    deploy: 81b108ec37bdcf670a4057f30e722e3c92efac01

---
+++ cofradia data/data.js
@@ -0,0 +1,12 @@
+export const items5 = [
+  {
+    id: 28,
+    nombre: "nombre1",
+    descripcion: "Costura cofrade, túnica y capirote de Ruan negro y capelina de niño de damasco negro y lazada de raso negra para el Grupo Parroquial de Ntra. Sra. del Sol (Málaga).",
+    fotos: [
+      "cofradia1a.jpeg",
+      "cofradia1b.jpeg"
+    ],
+    precio: "Consultar precio"
+  }
+]
